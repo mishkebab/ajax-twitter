a47a99613291b9ab2fa36bda1a5bdcf14d76a839
misha <mishabansal7@gmail.com>
2023-05-24 16:01:39 -0700

    started twitter ajax

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -2,11 +2,18 @@ import { API, broadcast } from "./util";
 
 export default class FollowToggle {
   constructor(toggleButton) {
-    // Your code here
+    this.toggleButton = toggleButton;
+    toggleButton.addEventListener("click", (this.handleClick.bind(this)));
   }
 
   async handleClick(event) {
     // Your code here
+    event.preventDefault();
+    if (this.followState === "followed") {
+      this.unfollow();
+    } else {
+      this.follow();
+    }
   }
 
   async follow() {
--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -1,10 +1,46 @@
+// const { custom } = require("babel-loader");
+
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
   options.headers = {
-    // Your code here
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "X-CSRF-Token": csrfToken,
     ...options.headers
   };
 
-  return await fetch(url, options);
+  const res = await fetch(url, options);
+  
+  if (res.ok) {
+    return await res.json();
+  } else {
+    throw res;
+  }
 }
+
+export const foo = "bar";
+
+export const followUser = (id) => {
+  return customFetch(`http://localhost:3000/users/${id}/follow`, 
+    {
+      method: "POST",
+      credentials: "include"
+    }  
+  )
+}
+
+export const unfollowUser = (id) => {
+  return customFetch(`http://localhost:3000/users/${id}/follow`, 
+    {
+      method: "DELETE",
+      credentials: "include"
+    }  
+  )
+}
+
+
+
+
+
+
